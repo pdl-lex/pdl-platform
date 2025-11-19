378d912394d4c09363f6dba2bce48a3b8e2913e8
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-19 16:33:38 +0100

    add main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+import "@mantine/core/styles.css"
+import { MantineProvider } from "@mantine/core"
+import { theme } from "./theme"
+import MainLayout from "./MainLayout"
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="auto">
+      <MainLayout />
+    </MantineProvider>
+  )
 }
