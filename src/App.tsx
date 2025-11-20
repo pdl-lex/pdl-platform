8a0f3712326676997486e224e81a6a67c2754c40
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-20 15:30:24 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import "@mantine/core/styles.css"
-import { MantineProvider, Text } from "@mantine/core"
+import { MantineProvider } from "@mantine/core"
 import { theme } from "./theme"
-import MainLayout from "./MainLayout"
+import MainLayout from "./layout/MainLayout"
+import { Route, Routes } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Corpora from "./pages/Corpora"
+import Dictionaries from "./pages/Dictionaries"
+import Lab from "./pages/Lab"
+import SearchDisplay from "./pages/SearchDisplay"
+
+const queryClient = new QueryClient()
+
+export type AppRoute = {
+  path: string
+  Component: React.ComponentType
+  title: string
+}
+
+const routes: AppRoute[] = [
+  { path: "/", Component: Home, title: "Startseite" },
+  { path: "/search", Component: SearchDisplay, title: "Suche" },
+  { path: "/dictionaries", Component: Dictionaries, title: "Wörterbücher" },
+  { path: "/corpora", Component: Corpora, title: "Korpora" },
+  { path: "/lab", Component: Lab, title: "Lab" },
+  { path: "/about", Component: About, title: "Über" },
+]
 
 export default function App() {
   return (
-    <MantineProvider theme={theme} defaultColorScheme="auto">
-      <MainLayout>
-        <Text>This is the main section, your app content here.</Text>
-        <Text>
-          Layout used in most cases – Navbar and Header with fixed position
-        </Text>
-      </MainLayout>
-    </MantineProvider> 
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme} defaultColorScheme="light">
+        <MainLayout routes={routes}>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+          </Routes>
+        </MainLayout>
+      </MantineProvider>
+    </QueryClientProvider>
   )
 }
