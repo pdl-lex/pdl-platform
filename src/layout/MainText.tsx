beb63878d5dda802bb87ed53dc6eb9cb48317e2c
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-05 17:35:18 +0100

    remove superfluous div

--- src/layout/MainText.tsx
+++ src/layout/MainText.tsx
@@ -3,9 +3,7 @@ import { Box, Typography } from "@mantine/core"
 export default function MainText({ children }: { children: React.ReactNode }) {
   return (
     <Box maw="800px" mx="auto" py="xl" px="md">
-      <Typography>
-        <div>{children}</div>
-      </Typography>
+      <Typography>{children}</Typography>
     </Box>
   )
 }
