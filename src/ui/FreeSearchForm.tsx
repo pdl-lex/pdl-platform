edb5076b815b26cee3811520e23c68baac03d13b
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-28 21:02:07 +0100

    add x button to clear textinput
    ref resets the focus back to the input

--- src/ui/FreeSearchForm.tsx
+++ src/ui/FreeSearchForm.tsx
@@ -1,7 +1,13 @@
-import { ActionIcon, Group, TextInput, GroupProps } from "@mantine/core"
+import {
+  ActionIcon,
+  Group,
+  TextInput,
+  GroupProps,
+  CloseButton,
+} from "@mantine/core"
 import { IconSearch } from "@tabler/icons-react"
 import { useNavigate, useSearchParams } from "react-router-dom"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export default function FreeSearchForm({ ...props }: GroupProps) {
   const navigate = useNavigate()
@@ -9,11 +15,24 @@ export default function FreeSearchForm({ ...props }: GroupProps) {
   const currentQuery = searchParams.get("q") || ""
 
   const [searchValue, setSearchValue] = useState(currentQuery)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const SearchButton = (
-    <ActionIcon variant="transparent" size="lg" type="submit">
-      <IconSearch size={16} />
-    </ActionIcon>
+    <ActionIcon.Group style={{ alignItems: "center" }}>
+      {searchValue && (
+        <CloseButton
+          variant="transparent"
+          size="sm"
+          onClick={() => {
+            setSearchValue("")
+            inputRef.current?.focus()
+          }}
+        />
+      )}
+      <ActionIcon variant="transparent" size="lg" type="submit">
+        <IconSearch size={16} />
+      </ActionIcon>
+    </ActionIcon.Group>
   )
 
   const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
@@ -27,7 +46,12 @@ export default function FreeSearchForm({ ...props }: GroupProps) {
   return (
     <Group component={"form"} onSubmit={handleSubmit} {...props}>
       <TextInput
+        ref={inputRef}
+        autoFocus
         rightSection={SearchButton}
+        rightSectionProps={{
+          style: { justifyContent: "flex-end" },
+        }}
         flex={1}
         placeholder="Freie Suche..."
         value={searchValue}
