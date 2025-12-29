ad9e5f195959424b58b56cf7acd9b2fcddb218cc
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-29 20:42:43 +0100

    debug resource option handling

--- src/ui/ComplexSearchForm.tsx
+++ src/ui/ComplexSearchForm.tsx
@@ -10,7 +10,7 @@ import { useForm } from "@mantine/form"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import { resources } from "../domain/Resource"
 import _ from "lodash"
-import { useEffect } from "react"
+import { useEffect, useMemo } from "react"
 
 const resourceKeys = Object.keys(resources)
 const resourceOptions = resourceKeys.map((key) => ({
@@ -19,7 +19,9 @@ const resourceOptions = resourceKeys.map((key) => ({
 }))
 
 function setResources(queryParameters: URLSearchParams, resources: string[]) {
-  const isDefaultSelection = resources.length === resourceKeys.length
+  const isDefaultSelection =
+    resources.length === resourceKeys.length || resources.length === 0
+  queryParameters.delete("resources")
   if (!isDefaultSelection) {
     resources.forEach((resourceKey: string) => {
       queryParameters.append("resources", resourceKey)
@@ -27,25 +29,34 @@ function setResources(queryParameters: URLSearchParams, resources: string[]) {
   }
 }
 
+function getResources(queryParameters: URLSearchParams): string[] {
+  const resources = queryParameters.getAll("resources")
+  return resources.length > 0 ? resources : resourceKeys
+}
+
 export default function FullSearchForm() {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const currentQuery = searchParams.get("q") || ""
+  const currentResources = useMemo(
+    () => getResources(searchParams),
+    [searchParams]
+  )
 
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
       query: currentQuery,
-      resources: resourceKeys,
+      resources: currentResources,
     },
   })
 
   useEffect(() => {
     form.setValues({
       query: currentQuery,
-      resources: resourceKeys,
+      resources: currentResources,
     })
-  }, [currentQuery])
+  }, [currentQuery, currentResources])
 
   const handleSubmit = (values: Record<string, any>) => {
     const queryParameters = new URLSearchParams({
