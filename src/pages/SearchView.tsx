48a65e9600272f480672cf5ddb9108eb5bea1ca9
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-01-09 17:38:52 +0100

    implement responsive two-column layout

--- src/pages/SearchView.tsx
+++ src/pages/SearchView.tsx
@@ -1,12 +1,24 @@
-import MainText from "../layout/MainText"
+import { Box, Grid } from "@mantine/core"
 import ComplexSearchForm from "../ui/ComplexSearchForm"
 import SearchResult from "../ui/SearchResult"
 
 export default function SearchView() {
   return (
-    <MainText>
-      <ComplexSearchForm />
-      <SearchResult />
-    </MainText>
+    <Grid>
+      <Grid.Col
+        span={{ base: 12, sm: 4 }}
+        mih={"100vh"}
+        style={{
+          borderRight: "1px solid var(--app-shell-border-color)",
+        }}
+      >
+        <Box p={"md"} style={{ position: "sticky", top: "61px" }}>
+          <ComplexSearchForm />
+        </Box>
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, xs: 8 }}>
+        <SearchResult />
+      </Grid.Col>
+    </Grid>
   )
 }
