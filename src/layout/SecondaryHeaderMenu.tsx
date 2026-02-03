a5afb34c0be012782b3b648ee86a604dbb5ad5da
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-02-03 17:10:57 +0100

    use HEADER_HEIGHT constant

--- src/layout/SecondaryHeaderMenu.tsx
+++ src/layout/SecondaryHeaderMenu.tsx
@@ -1,7 +1,12 @@
 import { Group } from "@mantine/core"
+import { HEADER_HEIGHT } from "./MainLayout"
 
 export default function SecondaryHeaderMenu() {
   return (
-    <Group h="50px" bg={"lexoterm-primary.9"} style={{ opacity: 0.75 }}></Group>
+    <Group
+      h={HEADER_HEIGHT * 0.25}
+      bg={"lexoterm-primary.9"}
+      style={{ opacity: 0.75 }}
+    ></Group>
   )
 }
