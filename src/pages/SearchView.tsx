0af3f2c03c18a587e7614e2b0019e0590df17f99
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-02-04 11:39:10 +0100

    apply ContentPanel to search filters

--- src/pages/SearchView.tsx
+++ src/pages/SearchView.tsx
@@ -1,18 +1,16 @@
-import { Box, Card, Grid, Title } from "@mantine/core"
+import { Box, Grid } from "@mantine/core"
 import ComplexSearchForm from "../ui/ComplexSearchForm"
 import SearchResult from "../ui/SearchResult"
+import ContentPanel from "../ui/ContentPanel"
 
 export default function SearchView() {
   return (
     <Grid maw={1200} p={"xl"} mx={"auto"}>
       <Grid.Col visibleFrom="sm" span={{ base: 12, sm: 4 }}>
         <Box>
-          <Card withBorder p={"lg"}>
-            <Title order={2} size="h4" mb="md">
-              Suchfilter
-            </Title>
+          <ContentPanel title="Suche">
             <ComplexSearchForm />
-          </Card>
+          </ContentPanel>
         </Box>
       </Grid.Col>
       <Grid.Col span={{ base: 12, sm: 4 }}>
