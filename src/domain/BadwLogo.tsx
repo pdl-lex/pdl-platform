79f734870c90d79067c30d0e2df21fdd4736642a
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-02-10 17:29:03 +0100

    always show BAdW logo in footer

--- src/domain/BadwLogo.tsx
+++ src/domain/BadwLogo.tsx
@@ -4,7 +4,6 @@ export default function BadwLogo() {
   return (
     <UnstyledButton component="a" href="/" rel="noopener noreferrer">
       <Image
-        visibleFrom="sm"
         src={"/badw_logo_black.svg"}
         alt="Logo der Bayerischen Akademie der Wissenschaften"
         h="auto"
