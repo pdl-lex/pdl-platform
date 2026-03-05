e9313c83fcb59204c62c35cd5386dc0bfb884ebd
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-03-05 20:20:41 +0100

    add ToolDetailModal

--- src/ui/ToolCard.tsx
+++ src/ui/ToolCard.tsx
@@ -1,15 +1,94 @@
-import { Card, Image, Title, Text, Group, Badge } from "@mantine/core"
-import { Tool } from "../domain/Tool"
+import {
+  Card,
+  Image,
+  Title,
+  Text,
+  Group,
+  Badge,
+  Modal,
+  Button,
+  alpha,
+  useMantineTheme,
+  SimpleGrid,
+  Typography,
+  Stack,
+} from "@mantine/core"
+import { Tool, ToolDetails, ToolImage } from "../domain/Tool"
+import { useDisclosure } from "@mantine/hooks"
 
 function LabelBar({ labels }: { labels: string[] }) {
   return (
-    <Group gap="xs">
+    <Group gap="xs" pt={"xs"}>
       {labels.map((label) => (
         <Badge radius="sm" size="xs" color="#8FB257">
           {label}
         </Badge>
       ))}
     </Group>
+  )
+}
+
+function hasDetails(tool: Tool): tool is Tool & { details: ToolDetails } {
+  return tool.details !== undefined
+}
+
+function ImageStack({ images }: { images: ToolImage[] }) {
+  return (
+    <Stack>
+      {images.map((image) => (
+        <Image src={image.url} alt={image.altText} />
+      ))}
+    </Stack>
+  )
+}
+
+function ToolDetailModal({ tool }: { tool: Tool & { details: ToolDetails } }) {
+  const [opened, { open, close }] = useDisclosure(false)
+  const theme = useMantineTheme()
+  const details = tool.details
+
+  return (
+    <>
+      <Modal
+        size={"xl"}
+        opened={opened}
+        onClose={close}
+        centered
+        closeButtonProps={{ style: { alignSelf: "flex-start" } }}
+        title={
+          <Stack gap={0}>
+            {tool.labels && <LabelBar labels={tool.labels} />}
+            <Title order={3} fw={500} pt={"sm"}>
+              {tool.details.title}
+            </Title>
+          </Stack>
+        }
+      >
+        {details.body && (
+          <SimpleGrid cols={{ base: 1, sm: 2 }}>
+            <Typography>
+              <div dangerouslySetInnerHTML={{ __html: details.body }} />
+            </Typography>
+            {details.images && details.images?.length > 0 && (
+              <ImageStack images={details.images} />
+            )}
+          </SimpleGrid>
+        )}
+      </Modal>
+      <Group>
+        <Button
+          variant="gradient"
+          onClick={open}
+          gradient={{
+            deg: 90,
+            from: "lexoterm-primary",
+            to: alpha(theme.colors["lexoterm-primary"][0], 0.75),
+          }}
+        >
+          Details
+        </Button>
+      </Group>
+    </>
   )
 }
 
@@ -20,10 +99,11 @@ export function ToolCard({ tool }: { tool: Tool }) {
         <Image src={tool.imageUrl} />
       </Card.Section>
       {tool.labels && <LabelBar labels={tool.labels} />}
-      <Title order={2} fw={500} fz={"lg"} py={"sm"}>
+      <Title order={2} fw={500} py={"sm"}>
         {tool.title}
       </Title>
-      <Text fz={"sm"}>{tool.teaser}</Text>
+      <Text pb={"md"}>{tool.teaser}</Text>
+      {hasDetails(tool) && <ToolDetailModal tool={tool} />}
     </Card>
   )
 }
