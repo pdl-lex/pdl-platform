36b6d720793e008a4ba958f4bab322091f9e2183
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-03-05 20:38:53 +0100

    add tool loading skeleton

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -1,4 +1,4 @@
-import { Grid, Loader } from "@mantine/core"
+import { Card, Grid, Group, Skeleton, Stack } from "@mantine/core"
 import { Tool } from "../domain/Tool"
 import { useQuery } from "@tanstack/react-query"
 import { ToolCard } from "../ui/ToolCard"
@@ -12,6 +12,30 @@ const fetchTools = async (): Promise<Tool[]> => {
   return (await response.json()) as Tool[]
 }
 
+function ToolPlaceholder() {
+  return (
+    <Grid.Col span={{ base: 12, sm: 4 }}>
+      <Card>
+        <Card.Section>
+          <Skeleton height={200} />
+        </Card.Section>
+        <Stack pt={"md"}>
+          <Group gap={"xs"}>
+            <Skeleton height={"1em"} w={"2em"} />
+            <Skeleton height={"1em"} w={"2em"} />
+            <Skeleton height={"1em"} w={"2em"} />
+          </Group>
+          <Skeleton height={"2em"} w={"50%"} />
+          <Skeleton height={"1em"} />
+          <Skeleton height={"1em"} />
+          <Skeleton height={"1em"} />
+          <Skeleton height={"2em"} w={"5em"} />
+        </Stack>
+      </Card>
+    </Grid.Col>
+  )
+}
+
 export default function Tools() {
   const { data, isFetching } = useQuery<Tool[]>({
     queryKey: ["tools"],
@@ -19,15 +43,21 @@ export default function Tools() {
     refetchOnWindowFocus: false,
     placeholderData: (previousData) => previousData,
   })
-  return isFetching ? (
-    <Loader />
-  ) : (
+  return (
     <Grid p={"md"} pt={"md"} mx={"auto"} gutter="xs">
-      {data?.map((tool) => (
-        <Grid.Col span={{ base: 12, sm: 4 }}>
-          <ToolCard tool={tool} />
-        </Grid.Col>
-      ))}
+      {isFetching ? (
+        <>
+          <ToolPlaceholder />
+          <ToolPlaceholder />
+          <ToolPlaceholder />
+        </>
+      ) : (
+        data?.map((tool) => (
+          <Grid.Col span={{ base: 12, sm: 4 }}>
+            <ToolCard tool={tool} />
+          </Grid.Col>
+        ))
+      )}
     </Grid>
   )
 }
