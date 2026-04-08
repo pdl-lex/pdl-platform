c65c90f46123479646d107db5832296961fc7a51
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-04-08 14:22:52 +0200

    implement accordion

--- src/ui/CmsFaqList.tsx
+++ src/ui/CmsFaqList.tsx
@@ -1,16 +1,22 @@
-import { Title } from "@mantine/core"
+import { Accordion, Title } from "@mantine/core"
 import CmsRichText from "./CmsRichText"
 import { FaqItem } from "../cms/blocks/FaqList"
 
 export default function CmsFaqList({ items }: { items: FaqItem[] }) {
   return (
-    <ul>
-      {items.map((item, index) => (
-        <li key={index}>
-          <Title order={3}>{item.question}</Title>
-          <CmsRichText data={item.answer} />
-        </li>
+    <Accordion variant={"default"}>
+      {items.map((item) => (
+        <Accordion.Item key={item.id} value={item.question}>
+          <Accordion.Control>
+            <Title mb={0} order={3} fw={500} fz={"h4"}>
+              {item.question}
+            </Title>
+          </Accordion.Control>
+          <Accordion.Panel pt={"md"}>
+            <CmsRichText data={item.answer} />
+          </Accordion.Panel>
+        </Accordion.Item>
       ))}
-    </ul>
+    </Accordion>
   )
 }
