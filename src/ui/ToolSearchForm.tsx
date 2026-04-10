24b2f28b37783696785b527e696dc1dff9230947
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-04-10 20:40:00 +0200

    add search fields for tool, author, tags

--- src/ui/ToolSearchForm.tsx
+++ src/ui/ToolSearchForm.tsx
@@ -1,10 +1,58 @@
-import { alpha, Button, Group, Stack, useMantineTheme } from "@mantine/core"
+import {
+  alpha,
+  Button,
+  Group,
+  MultiSelect,
+  Stack,
+  TextInput,
+  useMantineTheme,
+} from "@mantine/core"
+import { useForm } from "@mantine/form"
+
+const tagOptions = [
+  {
+    label: "VIS | Visualisierung",
+    value: "VIS",
+  },
+  {
+    label: "KLT | Korpuslinguistik und Textanalyse",
+    value: "KLT",
+  },
+]
 
 export default function ToolSearchForm() {
   const theme = useMantineTheme()
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: { tags: [], tool: "", author: "" },
+  })
+
   return (
     <form>
       <Stack>
+        <TextInput
+          key={form.key("tool")}
+          label={"Werkzeug"}
+          autoFocus
+          placeholder="Name, Beschreibung"
+          {...form.getInputProps("tool")}
+        />
+        <TextInput
+          key={form.key("author")}
+          label={"Autor:innen"}
+          autoFocus
+          placeholder="Autor:innen, Urheber:innen, Institutionen"
+          {...form.getInputProps("author")}
+        />
+        <MultiSelect
+          key={form.key("tags")}
+          label={"Kategorien"}
+          data={tagOptions}
+          placeholder={"Suche"}
+          searchable
+          {...form.getInputProps("tags")}
+        />
         <Group>
           <Button
             variant="gradient"
