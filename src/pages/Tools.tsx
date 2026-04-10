a404d4cb69e3635b24b11987dcbb41278aa19419
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-04-10 20:25:40 +0200

    add ToolSearchForm

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -3,6 +3,9 @@ import { Tool } from "../domain/Tool"
 import { useCmsCollection } from "../hooks/useCms"
 import { ToolCard } from "../ui/ToolCard"
 import { IconAlertCircle } from "@tabler/icons-react"
+import ContentPanel from "../ui/ContentPanel"
+import _ from "lodash"
+import ToolSearchForm from "../ui/ToolSearchForm"
 
 type ToolsCollectionResponse = {
   docs?: Tool[]
@@ -10,7 +13,7 @@ type ToolsCollectionResponse = {
 
 function ToolPlaceholder() {
   return (
-    <Grid.Col span={{ base: 12, sm: 4 }}>
+    <Grid.Col span={{ base: 12, sm: 6 }}>
       <Card>
         <Card.Section>
           <Skeleton height={200} />
@@ -40,37 +43,46 @@ export default function Tools() {
 
   return (
     <Grid p={"md"} pt={"md"} mx={"auto"} gutter="xs">
-      {isLoading ? (
-        <>
-          <ToolPlaceholder />
-          <ToolPlaceholder />
-          <ToolPlaceholder />
-        </>
-      ) : error ? (
-        <Grid.Col span={12}>
-          <Alert
-            title="Fehler beim Laden der Tools"
-            color="red"
-            icon={<IconAlertCircle size={18} />}
-          >
-            {error.message}
-          </Alert>
-        </Grid.Col>
-      ) : tools.length === 0 ? (
-        <Grid.Col span={12}>
-          <Alert
-            title="Keine Tools gefunden"
-            color="yellow"
-            icon={<IconAlertCircle size={18} />}
-          />
-        </Grid.Col>
-      ) : (
-        tools.map((tool) => (
-          <Grid.Col span={{ base: 12, sm: 4 }} key={tool.id}>
-            <ToolCard tool={tool} />
-          </Grid.Col>
-        ))
-      )}
+      <Grid.Col span={{ base: 12, sm: 4 }}>
+        <ContentPanel title="Suche">
+          <ToolSearchForm />
+        </ContentPanel>
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, sm: 8 }}>
+        <Grid>
+          {isLoading ? (
+            <>
+              {_.times(4, (index) => (
+                <ToolPlaceholder key={index} />
+              ))}
+            </>
+          ) : error ? (
+            <Grid.Col span={12}>
+              <Alert
+                title="Fehler beim Laden der Tools"
+                color="red"
+                icon={<IconAlertCircle size={18} />}
+              >
+                {error.message}
+              </Alert>
+            </Grid.Col>
+          ) : tools.length === 0 ? (
+            <Grid.Col span={12}>
+              <Alert
+                title="Keine Tools gefunden"
+                color="yellow"
+                icon={<IconAlertCircle size={18} />}
+              />
+            </Grid.Col>
+          ) : (
+            tools.map((tool) => (
+              <Grid.Col span={{ base: 12, sm: 6 }} key={tool.id}>
+                <ToolCard tool={tool} />
+              </Grid.Col>
+            ))
+          )}
+        </Grid>
+      </Grid.Col>
     </Grid>
   )
 }
---
+++ src/ui/ToolSearchForm.tsx
@@ -0,0 +1,3 @@
+export default function ToolSearchForm() {
+  return <>Search Form</>
+}
