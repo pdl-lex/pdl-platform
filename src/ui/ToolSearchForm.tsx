354fc46283182678db2c354cc8a927edda3aafcb
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-04-10 20:29:21 +0200

    set up form scaffold

--- src/ui/ToolSearchForm.tsx
+++ src/ui/ToolSearchForm.tsx
@@ -1,3 +1,34 @@
+import { alpha, Button, Group, Stack, useMantineTheme } from "@mantine/core"
+
 export default function ToolSearchForm() {
-  return <>Search Form</>
+  const theme = useMantineTheme()
+  return (
+    <form>
+      <Stack>
+        <Group>
+          <Button
+            variant="gradient"
+            gradient={{
+              deg: 90,
+              from: "lexoterm-secondary",
+              to: alpha(theme.colors["lexoterm-secondary"][0], 0.75),
+            }}
+          >
+            Zurücksetzen
+          </Button>
+          <Button
+            type="submit"
+            variant="gradient"
+            gradient={{
+              deg: 90,
+              from: "lexoterm-primary",
+              to: alpha(theme.colors["lexoterm-primary"][0], 0.75),
+            }}
+          >
+            Suchen
+          </Button>
+        </Group>
+      </Stack>
+    </form>
+  )
 }
