6636ce57a8d6ef418f8f594d22b272d0d2e8313f
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-06-05 16:52:09 +0200

    add search filter popover and floating button

--- src/pages/SearchView.tsx
+++ src/pages/SearchView.tsx
@@ -1,26 +1,32 @@
-import { Grid, Stack } from "@mantine/core"
+import {
+  ActionIcon,
+  Affix,
+  Text,
+  Drawer,
+  Grid,
+  Stack,
+  Indicator,
+  alpha,
+  useMantineTheme,
+  Box,
+} from "@mantine/core"
 import ComplexSearchForm from "../ui/ComplexSearchForm"
 import ContentPanel from "../ui/ContentPanel"
 import ResultSummary from "../ui/ResultSummary"
 import LemmaDetail from "../ui/LemmaDetail"
 import { useState } from "react"
 import KeywordList from "../ui/KeywordList"
+import { MENU_BREAKPOINT } from "../layout/MainLayout"
+import { useDisclosure } from "@mantine/hooks"
+import { IconFilter2Search } from "@tabler/icons-react"
+import { useSearchParams } from "react-router-dom"
 
 export default function SearchView() {
   const [activeLemmaId, setActiveLemmaId] = useState<string | null>(null)
-
-  return (
-    <Grid p={"md"} pt={"md"} mx={"auto"} gutter="xs">
-      <Grid.Col visibleFrom="sm" span={{ base: 12, sm: 4 }}>
-        <Stack gap={"xs"}>
-          <ContentPanel title="Suche">
-            <ComplexSearchForm />
-          </ContentPanel>
-          <ContentPanel title="Stichwortsuche">
-            <KeywordList />
-          </ContentPanel>
-        </Stack>
-      </Grid.Col>
+  const [filtersOpened, { open: openFilters, close: closeFilters }] =
+    useDisclosure(false)
+  const [searchParams] = useSearchParams()
+  const theme = useMantineTheme()
       <Grid.Col span={{ base: 12, sm: 4 }}>
         <ResultSummary
           activeLemmaId={activeLemmaId}
@@ -34,6 +40,50 @@ export default function SearchView() {
           </ContentPanel>
         )}
       </Grid.Col>
+      {searchParams.size !== 0 && (
+        <Affix position={{ bottom: 50, right: 50 }}>
+          <ActionIcon
+            hiddenFrom={MENU_BREAKPOINT}
+            size="lg"
+            variant={"gradient"}
+            gradient={{
+              deg: 90,
+              from: "lexoterm-primary",
+              to: alpha(theme.colors["lexoterm-primary"][0], 0.75),
+            }}
+            radius={"xl"}
+            p={"lg"}
+            aria-label="Settings"
+            onClick={openFilters}
+          >
+            <Indicator
+              offset={5}
+              disabled={searchParams.size === 0}
+              size={8}
+              color={"lexoterm-secondary.0"}
+            >
+              <IconFilter2Search stroke={1.5} />
+            </Indicator>
+          </ActionIcon>
+        </Affix>
+      )}
+      <Drawer
+        offset={8}
+        title={
+          <Text size="xl" fw="bold">
+            Suchfilter
+          </Text>
+        }
+        radius="md"
+        size="lg"
+        padding="lg"
+        opened={filtersOpened}
+        position={"top"}
+        overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+        onClose={closeFilters}
+      >
+        <ComplexSearchForm onSubmit={closeFilters} />
+      </Drawer>
     </Grid>
   )
 }
