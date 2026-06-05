ebe16c333734e2e4d27283b47292dffa418b65cf
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-06-05 16:50:16 +0200

    remove search filters from top navigation

--- src/layout/PrimaryHeaderMenu.tsx
+++ src/layout/PrimaryHeaderMenu.tsx
@@ -1,24 +1,17 @@
 import {
-  ActionIcon,
   Box,
   Burger,
-  Drawer,
   Flex,
   getGradient,
   Group,
-  Indicator,
-  Text,
   UnstyledButton,
   useMantineTheme,
 } from "@mantine/core"
-import { NavLink, useSearchParams } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import classes from "./MainLayout.module.css"
 import { AppRoute } from "../App"
 import LexoTermLogo from "./LexoTermLogo"
 import classNames from "classnames"
-import { IconFilter2Search } from "@tabler/icons-react"
-import { useDisclosure } from "@mantine/hooks"
-import ComplexSearchForm from "../ui/ComplexSearchForm"
 import { HEADER_HEIGHT, MENU_BREAKPOINT } from "./MainLayout"
 
 export default function PrimaryHeaderMenu({
@@ -30,10 +23,6 @@ export default function PrimaryHeaderMenu({
   opened: boolean
   toggle: () => void
 }) {
-  const [filtersOpened, { open: openFilters, close: closeFilters }] =
-    useDisclosure(false)
-
-  const [searchParams] = useSearchParams()
   const theme = useMantineTheme()
 
   return (
@@ -55,6 +44,7 @@ export default function PrimaryHeaderMenu({
         maw={"1440px"}
         mx="auto"
       >
+        <LexoTermLogo />
         <Burger
           opened={opened}
           onClick={toggle}
@@ -62,7 +52,6 @@ export default function PrimaryHeaderMenu({
           size="md"
           color="white"
         />
-        <LexoTermLogo />
         <Group ml="xl" gap={0} visibleFrom={MENU_BREAKPOINT}>
           {routes.map(({ path, title }) => (
             <UnstyledButton
@@ -88,35 +77,6 @@ export default function PrimaryHeaderMenu({
             </UnstyledButton>
           ))}
         </Group>
-        <ActionIcon
-          style={{ color: "inherit" }}
-          hiddenFrom={MENU_BREAKPOINT}
-          size="lg"
-          variant="subtle"
-          aria-label="Settings"
-          onClick={openFilters}
-        >
-          <Indicator offset={5} disabled={searchParams.size === 0} size={6}>
-            <IconFilter2Search stroke={1.5} />
-          </Indicator>
-        </ActionIcon>
-        <Drawer
-          offset={8}
-          title={
-            <Text size="xl" fw="bold">
-              Suchfilter
-            </Text>
-          }
-          radius="md"
-          size="lg"
-          padding="lg"
-          opened={filtersOpened}
-          position={"top"}
-          overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
-          onClose={closeFilters}
-        >
-          <ComplexSearchForm onSubmit={closeFilters} />
-        </Drawer>
       </Flex>
     </Box>
   )
